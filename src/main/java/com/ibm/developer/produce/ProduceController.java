c71e7d29ec03e8ed8b90518ea56408a8cbf27fea
Billy Korando <wkorando@gmail.com>
2020-02-03 13:08:24 +0100

    Adding downstream stuff

--- src/main/java/com/ibm/developer/produce/ProduceController.java
+++ src/main/java/com/ibm/developer/produce/ProduceController.java
@@ -1,5 +1,7 @@
 package com.ibm.developer.produce;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/v1/produce")
@@ -15,24 +18,38 @@ public class ProduceController {
 
 	private ProduceRepo repo;
 	private ProduceService service;
+	private RestTemplate restTemplate;
+	private static final Logger LOGGER = LoggerFactory.getLogger(ProduceController.class);
 
-	public ProduceController(ProduceRepo repo, ProduceService service) {
+	public ProduceController(ProduceRepo repo, ProduceService service, RestTemplate restTemplate) {
+		super();
 		this.repo = repo;
 		this.service = service;
+		this.restTemplate = restTemplate;
 	}
 
 	@GetMapping
 	public ResponseEntity<Iterable<Produce>> findAllProduce() {
+		LOGGER.info("Getting all produce");
 		return ResponseEntity.ok(repo.findAll());
 	}
 
 	@GetMapping("/{name}")
 	public ResponseEntity<Iterable<Produce>> findProduceByName(@PathVariable String name) {
+		LOGGER.info("Getting all produce that matches: " + name);
+		try {
+			String message = restTemplate.getForObject("/api/v1/downstream", String.class);
+			LOGGER.info("Message from downstream service: " + message);
+		} catch (Exception e) {
+			//EATING exception for now, fix later
+		}
+
 		return ResponseEntity.ok(service.findProduceByName(name));
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<Produce> addNewProduce(@RequestBody Produce produce) {
+		LOGGER.info("Adding new produce: " + produce.toString());
 		return ResponseEntity.ok(service.addNewProduce(produce));
 	}
 
