d48c03cba4f3caab9f0a0d4f4c8318e29a3822a4
Billy Korando <wkorando@gmail.com>
2020-05-05 13:53:54 -0500

    Final updates

--- src/main/java/com/ibm/developer/produce/ProduceService.java
+++ src/main/java/com/ibm/developer/produce/ProduceService.java
@@ -1,21 +1,32 @@
 package com.ibm.developer.produce;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProduceService {
 
 	private ProduceRepo repo;
-
+//	private KafkaTemplate<String, Produce> template;
+	
+//	@Autowired(required = false)
 	public ProduceService(ProduceRepo repo) {
 		this.repo = repo;
 	}
+	
+//	@Autowired(required = false)
+//	public ProduceService(ProduceRepo repo, KafkaTemplate<String, String> template) {
+//		this.repo = repo;
+//	}
 
 	public Produce addNewProduce(Produce produce) {
 		if (produce.getName().isEmpty() || !(produce.getQuantity() > 0)) {
 			throw new ClientException("Missing required value!");
 		}
-		return repo.save(produce);
+		
+//		template.sendDefault(produce);
+		
+		return produce;
 	}
 
 	public Iterable<Produce> findProduceByName(String name) {
